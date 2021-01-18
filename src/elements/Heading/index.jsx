fe1d375ae48f847c3448d6e22b14b9a5da6cbf29
Susan <susanwenzhang@gmail.com>
2021-01-18 15:43:18 -0500

    feat(styles): fixed alignment on headings

--- src/elements/Heading/index.jsx
+++ src/elements/Heading/index.jsx
@@ -1,22 +1,21 @@
 import { H1, H2, H3, H4 } from "./styles"
 
-export default function Heading({ size, colour, align, children }) {
-  const headingColour = colour ? colour : `dark-neutral`
+export default function Heading({ size, invert, align, children }) {
   const headingSize = size ? size : 'H1'
   const headingAlignment = align ? align : 'center'
   let headingBody;
   switch (headingSize) {
     case "H1":
-      headingBody = <H1 colour={headingColour} alignment={headingAlignment}>{children}</H1>
+      headingBody = <H1 invert={invert} align={headingAlignment}>{children}</H1>
       break
     case "H2":
-      headingBody = <H2 colour={headingColour} alignment={headingAlignment}>{children}</H2>
+      headingBody = <H2 invert={invert} align={headingAlignment}>{children}</H2>
       break
     case "H3":
-      headingBody = <H3 colour={headingColour} alignment={headingAlignment}>{children}</H3>
+      headingBody = <H3 invert={invert} align={headingAlignment}>{children}</H3>
       break
     case "H4":
-      headingBody = <H4 colour={headingColour} alignment={headingAlignment}>{children}</H4>
+      headingBody = <H4 invert={invert} align={headingAlignment}>{children}</H4>
       break
     default:
       headingBody = null
--- src/elements/Heading/styles.jsx
+++ src/elements/Heading/styles.jsx
@@ -2,7 +2,16 @@ import styled from 'styled-components';
 import breakpoint from "../../styles/breakpoints"
 
 const H1 = styled.h1`
-  color: ${props => props.colour === "dark-neutral" ? props.theme.darkNeutral : props.theme.white};
+  color: ${props => props.invert ? props.theme.white : props.theme.darkNeutral};
+  text-align: ${props => {
+    if (props.align === 'left') {
+      return 'left'
+    } else if (props.align === 'right') {
+      return 'right'
+    } else {
+      return 'center'
+    }
+  }};
   font-size: 36px;
   letter-spacing: -0.5px;
   font-weight: 700;
@@ -12,7 +21,16 @@ const H1 = styled.h1`
 `
 
 const H2 = styled.h2`
-  color: ${props => props.colour === "dark-neutral" ? props.theme.darkNeutral : props.theme.white};
+  color: ${props => props.invert ? props.theme.white : props.theme.darkNeutral};
+  text-align: ${props => {
+    if (props.align === 'left') {
+      return 'left'
+    } else if (props.align === 'right') {
+      return 'right'
+    } else {
+      return 'center'
+    }
+  }};
   font-size: 30px;
   font-weight: 500;
   @media ${breakpoint.lg} {
@@ -21,7 +39,16 @@ const H2 = styled.h2`
 `
 
 const H3 = styled.h3`
-  color: ${props => props.colour === "dark-neutral" ? props.theme.darkNeutral : props.theme.white};
+  color: ${props => props.invert ? props.theme.white : props.theme.darkNeutral};
+  text-align: ${props => {
+    if (props.align === 'left') {
+      return 'left'
+    } else if (props.align === 'right') {
+      return 'right'
+    } else {
+      return 'center'
+    }
+  }};
   font-size: 26px;
   letter-spacing: 0.25px;
   font-weight: 500;
@@ -31,7 +58,16 @@ const H3 = styled.h3`
 `
 
 const H4 = styled.h4`
-  color: ${props => props.colour === "dark-neutral" ? props.theme.darkNeutral : props.theme.white};
+  color: ${props => props.invert ? props.theme.white : props.theme.darkNeutral};
+  text-align: ${props => {
+    if (props.align === 'left') {
+      return 'left'
+    } else if (props.align === 'right') {
+      return 'right'
+    } else {
+      return 'center'
+    }
+  }};
   font-size: 22px;
   letter-spacing: 0.25px;
   @media ${breakpoint.lg} {
