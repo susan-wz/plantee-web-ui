642fbdcfa80ea6da932db470c4af135cd1e542ee
Susan <susanwenzhang@gmail.com>
2021-01-17 20:57:28 -0500

    feat(styles): add styled components

--- src/App.js
+++ src/App.js
@@ -6,47 +6,51 @@ import {
   Link
 } from "react-router-dom";
 import { AddPlants, Explore, MyPlants, Settings, Tasks } from "./pages"
+import { ThemeProvider } from 'styled-components';
+import theme from "./theme"
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Tasks</Link>
-          </li>
-          <li>
-            <Link to="/explore">Explore Plants</Link>
-          </li>
-          <li>
-            <Link to="/my_plants">My Plants</Link>
-          </li>
-          <li>
-            <Link to="/add_plants">Add Plants</Link>
-          </li>
-          <li>
-            <Link to="/settings">Settings</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/">
-          <Tasks />
-        </Route>
-        <Route path="/explore">
-          <Explore />
-        </Route>
-        <Route path="/my_plants">
-          <MyPlants />
-        </Route>
-        <Route path="/add_plants">
-          <AddPlants />
-        </Route>
-        <Route path="/settings">
-          <Settings />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Tasks</Link>
+            </li>
+            <li>
+              <Link to="/explore">Explore Plants</Link>
+            </li>
+            <li>
+              <Link to="/my_plants">My Plants</Link>
+            </li>
+            <li>
+              <Link to="/add_plants">Add Plants</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <Tasks />
+          </Route>
+          <Route path="/explore">
+            <Explore />
+          </Route>
+          <Route path="/my_plants">
+            <MyPlants />
+          </Route>
+          <Route path="/add_plants">
+            <AddPlants />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  primary: "008148", 
+  primaryLight: "B8FFBB",
+  secondary: "285CA4", 
+  secondaryLight: "BDD3EF", 
+  accent: "FCCA46",
+  accentLight: "FEE39A", 
+  success: "008148", 
+  danger: "A22522", 
+  warning: "FCCA46", 
+  darkNeutral: "34312D", 
+  mediumNeutral: "DBD3D8", 
+  white: "FFFFFF"
+}
+
+export default theme;
