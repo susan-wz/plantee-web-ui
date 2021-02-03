d075d90b7a27569bfe549d129ce68b94770ba2a0
Susan <susanwenzhang@gmail.com>
2021-02-02 23:33:22 -0500

    feat(elements): updating card styles

--- src/elements/Card/styles.jsx
+++ src/elements/Card/styles.jsx
@@ -1,20 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledCard = styled.div`
   border-radius: 8px;
-  width: ${props => props.width ? props.width : "100%"};
-  border: ${props => props.border ? `1px solid ${props.theme.mediumNeutral}` : null};
-  box-shadow: ${props => props.shadow ? "0 0 8px rgba(0, 0, 0, 0.1)" : null};
-  background-color: ${props => {
+  width: ${(props) => (props.width ? props.width : "100%")};
+  border: ${(props) =>
+    props.border ? `1px solid ${props.theme.mediumNeutral}` : null};
+  box-shadow: ${(props) =>
+    props.shadow ? "0 0 8px rgba(0, 0, 0, 0.1)" : null};
+  background-color: ${(props) => {
     if (props.background === "secondary") {
-      return props.theme.secondary
+      return props.theme.secondary;
     } else if (props.background === "accent") {
-      return props.theme.accent
+      return props.theme.accent;
     } else if (props.background === "primary") {
-      return props.theme.primary
+      return props.theme.primary;
+    } else if (props.background === "white") {
+      return props.theme.white;
     } else {
       return null;
-    }}}
-`
+    }
+  }};
+`;
 
-export default StyledCard
+export default StyledCard;
