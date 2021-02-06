711a9f68064656cfd720d1aabd04d1b5b05daa83
Susan <susanwenzhang@gmail.com>
2021-02-05 23:17:46 -0500

    test(paragraph): add tests for paragraph

--- src/elements/Paragraph/__tests__/Paragraph.spec.jsx
+++ src/elements/Paragraph/__tests__/Paragraph.spec.jsx
@@ -1,11 +1,28 @@
-import { shallow } from "enzyme"
-import Paragraph from "../index"
+import { shallow, mount } from "enzyme";
+import Paragraph from "../index";
+import "jest-styled-components";
 
-const paragraphText = "Test paragraph"
+const paragraphText = "Test paragraph";
 
 describe("Paragraph", () => {
   it("should render the paragraph component with text", () => {
-    let wrapper = shallow(<Paragraph>{paragraphText}</Paragraph>)
-    expect(wrapper.find("Paragraph").text()).toEqual(paragraphText)
+    let wrapper = shallow(<Paragraph>{paragraphText}</Paragraph>);
+    expect(wrapper.find("Paragraph").text()).toEqual(paragraphText);
+  });
+  it("should be centre aligned if centre alignment is specified", () => {
+    let wrapper = mount(<Paragraph align="center">{paragraphText}</Paragraph>);
+    expect(wrapper).toHaveStyleRule("text-align", "center");
+  });
+  it("should be right aligned if right alignment is specified", () => {
+    let wrapper = mount(<Paragraph align="right">{paragraphText}</Paragraph>);
+    expect(wrapper).toHaveStyleRule("text-align", "right");
+  });
+  it("should be 20px if size large is specified", () => {
+    let wrapper = mount(<Paragraph size="large">{paragraphText}</Paragraph>);
+    expect(wrapper).toHaveStyleRule("font-size", "20px");
   })
-})
+  it("should be 12px if size small is specified", () => {
+    let wrapper = mount(<Paragraph size="small">{paragraphText}</Paragraph>);
+    expect(wrapper).toHaveStyleRule("font-size", "12px");
+  })
+});
