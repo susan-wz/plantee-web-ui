d53792396abaab1db9149dc60e69a7ac38de3bc7
Susan <susanwenzhang@gmail.com>
2021-02-05 23:17:02 -0500

    test(caption): add tests for caption

--- src/elements/Caption/__tests__/Caption.spec.jsx
+++ src/elements/Caption/__tests__/Caption.spec.jsx
@@ -1,5 +1,6 @@
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import Caption from "../index"
+import "jest-styled-components";
 
 const captionText = "Test caption"
 
@@ -8,4 +9,16 @@ describe("Caption", () => {
     let wrapper = shallow(<Caption>{captionText}</Caption>)
     expect(wrapper.find("Caption").text()).toEqual(captionText)
   })
+  it("should be centre aligned if centre alignment is specified", () => {
+    let wrapper = mount(<Caption align="center">{captionText}</Caption>);
+    expect(wrapper).toHaveStyleRule("text-align", "center");
+  });
+  it("should be right aligned if right alignment is specified", () => {
+    let wrapper = mount(<Caption align="right">{captionText}</Caption>);
+    expect(wrapper).toHaveStyleRule("text-align", "right");
+  });
+  it("should be left aligned if no alignment is specified", () => {
+    let wrapper = mount(<Caption>{captionText}</Caption>);
+    expect(wrapper).toHaveStyleRule("text-align", "left");
+  });
 })
